17e883b7426830f6f6aeb169273a29956c9acf38
HarshiniMallepalle <harshinim0802@gmail.com>
2023-04-24 08:14:42 -0400

    New

--- src/test/java/com/training/annotation/Test1.java
+++ src/test/java/com/training/annotation/Test1.java
@@ -8,7 +8,7 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 /*
- * Testing for Git Monday Testing
+ * We do changes in Branch 
  * 
  * 
  */
